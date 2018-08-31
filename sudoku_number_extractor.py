0bd647c4934995e45b86548a25c150db06728962
sksenthilkumar <sksenthilkumar93@gmail.com>
2018-08-31 18:18:15 +0200

    implemented homography

--- sudoku_number_extractor.py
+++ sudoku_number_extractor.py
@@ -1,20 +1,52 @@
 import cv2
-
+import numpy as np
+import math
 import argparse
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", help="image location", default= 'images/sudoku.png')
+parser.add_argument("-i", help="image location", default='images/sudoku.png')
 parser.add_argument("-s", help="Show_image_processing", default=True)
 args = parser.parse_args()
 image_name = args.i
-Display = args.s
+Display = str2bool(args.s)
+IMAGE_WIDHT = 16
+IMAGE_HEIGHT = 16
+SUDOKU_SIZE= 9
+N_MIN_ACTVE_PIXELS = 10
 
 
 def show_images(name, img):
     if Display:
         cv2.imshow(name, img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+
+def getOuterPoints(rcCorners):
+    ar = []
+    ar.append(rcCorners[0, 0, :])
+    ar.append(rcCorners[1, 0, :])
+    ar.append(rcCorners[2, 0, :])
+    ar.append(rcCorners[3, 0, :])
+
+    x_sum = sum(rcCorners[x, 0, 0] for x in range(len(rcCorners))) / len(rcCorners)
+    y_sum = sum(rcCorners[x, 0, 1] for x in range(len(rcCorners))) / len(rcCorners)
+
+    def algo(v):
+        return (math.atan2(v[0] - x_sum, v[1] - y_sum)+ 2 * math.pi) % 2 * math.pi
+
+    ar.sort(key=algo)
+
+    return (ar[0], ar[3], ar[2], ar[1])
 
 
 image_sudoku_original = cv2.imread(image_name)
@@ -24,17 +56,15 @@ gray = cv2.cvtColor(image_sudoku_original, cv2.COLOR_BGR2GRAY)
 gray = cv2.bilateralFilter(gray, 11, 17, 17)
 show_images('bilateralFilter', gray)
 
-thresh = cv2.adaptiveThreshold(gray,255,1,1,11,15)
+thresh = cv2.adaptiveThreshold(gray,255, 1, 1, 11, 15)
 show_images('threshold', thresh)
 
 # applying canny edge detection doesn't work for finding the sudoku box
-#edged = cv2.Canny(thresh, 30, 200)
-#show_images('cannyEdge', edged)
+# edged = cv2.Canny(thresh, 30, 200)
+# show_images('cannyEdge', edged)
 
 __ , contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-print(len(contours))
 contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
-print(len(contours))
 
 rough_note = image_sudoku_original.copy()
 cv2.drawContours(rough_note, contours, -1, (0, 255, 0), 3)
@@ -47,7 +77,6 @@ for contour in contours:
     # aproximate countours to polygons
     perimeter = cv2.arcLength(contour, True)
     approximation = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
-
 
     # has the polygon 4 sides?
     if not (len(approximation) == 4):
@@ -66,5 +95,22 @@ for contour in contours:
 rough_note = image_sudoku_original.copy()
 cv2.drawContours(rough_note, [big_rectangle], 0, (0, 255, 0), 3)
 show_images('Sudoku', rough_note)
+
+points1 = np.array([
+                    np.array([0.0, 0.0], np.float32) + np.array([144, 0], np.float32),
+                    np.array([0.0, 0.0], np.float32),
+                    np.array([0.0, 0.0], np.float32) + np.array([0.0, 144], np.float32),
+                    np.array([0.0, 0.0], np.float32) + np.array([144, 144], np.float32),
+                    ],np.float32)
+
+outerPoints = getOuterPoints(big_rectangle)
+points2 = np.array(outerPoints, np.float32)
+pers = cv2.getPerspectiveTransform(points2,  points1)
+
+warp = cv2.warpPerspective(image_sudoku_original, pers, (SUDOKU_SIZE*IMAGE_HEIGHT, SUDOKU_SIZE*IMAGE_WIDHT))
+warp_gray = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
+
+show_images('transformed', warp_gray)
+
 
 
